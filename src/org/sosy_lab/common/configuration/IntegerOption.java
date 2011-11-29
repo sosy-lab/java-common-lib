26e32ad89c6f7a9024da1366b0047a2fd848d3db
Philipp Wendler <uni@philippwendler.de>
2011-11-29 17:36:29 +0000

    Another special option annotation for integer/long options which gets the min and max fields from the @Option annotation.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@163 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/configuration/IntegerOption.java
@@ -0,0 +1,55 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2011  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.common.configuration;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * This is an optional annotation for all configuration options (i.e., elements
+ * that are annotated with {@link Option}) whose type is an integer number
+ * (i.e., int, long, Integer and Long):
+ *
+ * It serves to specify minimal and/or maximal values.
+ */
+@OptionDetailAnnotation(applicableTo= {int.class, long.class, Integer.class, Long.class})
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface IntegerOption {
+
+  /**
+   * An optional minimum value for this option.
+   */
+  public long min() default Long.MIN_VALUE;
+
+  /**
+   * An optional maximum value for this option.
+   */
+  public long max() default Long.MAX_VALUE;
+}
