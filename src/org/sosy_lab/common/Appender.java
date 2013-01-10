2d58ebb6db5c78b26e81c1b4345bf0c89c8b8f94
pwendler <pwendler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2013-01-10 10:15:45 +0000

    New interface Appender that makes it possible to use helper methods for I/O and still write continuosly into the file while creating the string representation instead of first creating the complete string and then dumping it into the file at once.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@232 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/Appender.java
@@ -0,0 +1,46 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2013  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.common;
+
+import java.io.IOException;
+
+/**
+ * An interface for classes that know how to dump themselves into an {@link Appendable}.
+ * This can be used for large string outputs, where writing into an Appendable
+ * is faster than creating a string and then writing it in one piece.
+ */
+public interface Appender {
+
+  /**
+   * Writes a string representation of this object into the given {@link Appendable}.
+   *
+   * It is strongly encouraged that this method behaves identically to
+   * <code>appendable.append(this.toString())</code>,
+   * except for possibly calling append multiple times with bits of the output.
+   *
+   * @param appendable The appendable, may not be null.
+   * @throws IOException If the appendable throws an IOException
+   */
+  void appendTo(Appendable appendable) throws IOException;
+}
