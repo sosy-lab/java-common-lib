72cee27be2fa2f902c3073258f79c1a6d7c9c2ce
pwendler <pwendler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2013-08-21 13:38:01 +0000

    Add a logging implementation that avoids duplicate messages by storing them and printing each message only once.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@267 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/log/LogManagerWithoutDuplicates.java
@@ -0,0 +1,110 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2013  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.common.log;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * LogManager implementation which helps to get log messages printed only once,
+ * and avoid duplicate messages.
+ */
+public class LogManagerWithoutDuplicates extends ForwardingLogManager
+                                         implements LogManager {
+
+  private final LogManager delegate;
+
+  private final Set<ImmutableList<Object>> seenMessages = new HashSet<>();
+
+  public LogManagerWithoutDuplicates(LogManager pDelegate) {
+    delegate = checkNotNull(pDelegate);
+  }
+
+  @Override
+  protected LogManager delegate() {
+    return delegate;
+  }
+
+  /**
+   * Logging method similar to {@link #log(Level, Object...)},
+   * however, subsequent calls to this method with the same arguments
+   * will be silently ignored.
+   * Direct calls to {@link #log(Level, Object...)} are not affected.
+   *
+   * Make sure to call this method only with immutable parameters,
+   * such as Strings!
+   * If objects are changed after being passed to this method,
+   * detecting duplicate log messages may not work,
+   * or too many log messages may be ignored.
+   */
+  public void logOnce(Level pPriority, Object... pArgs) {
+    if (wouldBeLogged(pPriority)) {
+
+      if (seenMessages.add(ImmutableList.copyOf(pArgs))) {
+        // log only if not already seen
+        log(pPriority, pArgs);
+      }
+    }
+  }
+
+  /**
+   * Logging method similar to {@link #logf(Level, String, Object...)},
+   * however, subsequent calls to this method with the same arguments
+   * will be silently ignored.
+   * Direct calls to {@link #logf(Level, String, Object...)} are not affected.
+   *
+   * Make sure to call this method only with immutable parameters,
+   * such as Strings!
+   * If objects are changed after being passed to this method,
+   * detecting duplicate log messages may not work,
+   * or too many log messages may be ignored.
+   */
+  public void logfOnce(Level pPriority, String pFormat, Object... pArgs) {
+    if (wouldBeLogged(pPriority)) {
+
+      ImmutableList.Builder<Object> args = ImmutableList.builder();
+      args.add(pFormat);
+      args.add(pArgs);
+
+      if (seenMessages.add(args.build())) {
+        // log only if not already seen
+        logf(pPriority, pFormat, pArgs);
+      }
+    }
+  }
+
+  /**
+   * Reset all seen log messages, such that {@link #logfOnce(Level, String, Object...)}
+   * and {@link #logfOnce(Level, String, Object...)} will be guaranteed
+   * to behave exactly like in a fresh instance of this class.
+   */
+  public void resetSeenMessages() {
+    seenMessages.clear();
+  }
+}
