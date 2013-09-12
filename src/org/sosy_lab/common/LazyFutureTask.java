9f909209d81412a988b721ce55fe88f211c40685
pwendler <pwendler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2013-09-12 14:02:01 +0000

    Implementation of Future that can be used to execute tasks on the same thread as the caller (like with MoreExecutors#sameThreadExecutor()), but lazily when Future#get() is called instead of when ExecutorService#submit() is called.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@273 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/LazyFutureTask.java
@@ -0,0 +1,88 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2013  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.common;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Future implementation that can be used when a task should be executed
+ * only lazily at the first time {@link #get()} is called.
+ * I.e., it is not guaranteed that the task is run at all,
+ * but it is called at most once.
+ *
+ * Execution of the task happens in the caller's thread,
+ * a little bit similar to the use of
+ * {@link com.google.common.util.concurrent.MoreExecutors#sameThreadExecutor()},
+ * however, it is executed on the thread calling {@link #get()}
+ * and not on the thread calling
+ * {@link java.util.concurrent.ExecutorService#submit(Runnable)}.
+ *
+ * Important: Calling {@link #get(long, TimeUnit)} is not supported
+ * and will always throw {@link UnsupportedOperationException}.
+ *
+ * Canceling this future works as expected.
+ */
+public class LazyFutureTask<V> extends FutureTask<V> {
+
+  public LazyFutureTask(Callable<V> pCallable) {
+    super(pCallable);
+  }
+
+  public LazyFutureTask(Runnable pRunnable, V pResult) {
+    super(pRunnable, pResult);
+  }
+
+  @Override
+  public void run() {
+    // Do nothing here, we execute the task only lazily in get().
+  }
+
+  @Override
+  public V get() throws InterruptedException, ExecutionException {
+    if (!isDone()) {
+      // Note that two threads calling this method at the same time is safe
+      // (the task won't actually be executed twice)
+      // because super.run() checks whether the future is currently in state
+      // RUNNING and does nothing in this case.
+      // (This is an advantage over using Guava's AbstractFuture,
+      // where we would have to do this ourselves.)
+      super.run();
+    }
+
+    return super.get();
+  }
+
+  /**
+   * Always throws {@link UnsupportedOperationException}.
+   * @throws UnsupportedOperationException
+   */
+  @Deprecated
+  @Override
+  public V get(long pTimeout, TimeUnit pUnit) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException();
+  }
+}
