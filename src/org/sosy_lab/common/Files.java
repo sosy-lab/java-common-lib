5f48f383ea16b556999893c61300dd1d5c10ccb4
dresler <dresler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2013-11-11 14:43:30 +0000

    Replace all occurrences of java.nio.file.Path(s) and java.nio.file.Files.
    This is necessary to run on Google App Engine as the mentioned classes
    are not on Google's whitelist.
    
    git-svn-id: https://svn.sosy-lab.org/software/common/branches/google-appengine@328 05a39ec8-3e86-40da-8f8d-20173f296dbc

--- src/org/sosy_lab/common/Files.java
+++ src/org/sosy_lab/common/Files.java
@@ -20,7 +20,6 @@
 package org.sosy_lab.common;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static java.nio.file.StandardOpenOption.*;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -28,15 +27,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.Charset;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.nio.file.attribute.FileAttribute;
 
 import javax.annotation.Nullable;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.io.FileWriteMode;
 
 /**
  * Provides helper functions for file access.
@@ -70,7 +66,7 @@ public final class Files {
       } catch (IOException e) {
         // creation was successful, but writing failed
         // -> delete file
-        delete(file.toPath(), e);
+        delete(Path.fromFile(file), e);
 
         throw e;
       }
@@ -80,7 +76,7 @@ public final class Files {
 
   /**
    * Create a temporary file similar to
-   * {@link java.nio.file.Files#createTempFile(String, String, FileAttribute...)}.
+   * {@link java.io.File#createTempFile(String, String)}.
    *
    * The resulting {@link Path} object is wrapped in a {@link DeleteOnCloseFile},
    * which deletes the file as soon as {@link DeleteOnCloseFile#close()} is called.
@@ -92,44 +88,42 @@ public final class Files {
    * }
    * </code>
    *
-   * The difference to using {@link StandardOpenOption#DELETE_ON_CLOSE} is that
-   * the file can be opened and closed multiple times,
-   * potentially from different processes.
+   * The file can be opened and closed multiple times, potentially from different processes.
    *
    * @param prefix
    * @param suffix
    * @return
    * @throws IOException
    */
-  public static DeleteOnCloseFile createTempFile(@Nullable String prefix, @Nullable String suffix,
-      FileAttribute<?>... attrs) throws IOException {
-    return new DeleteOnCloseFile(java.nio.file.Files.createTempFile(prefix, suffix, attrs));
+  public static DeleteOnCloseFile createTempFile(@Nullable String prefix, @Nullable String suffix) throws IOException {
+    Path tempFile = Path.fromFile(File.createTempFile(prefix, suffix));
+    return new DeleteOnCloseFile(tempFile);
   }
 
   /**
    * A simple wrapper around {@link Path} that calls
-   * {@link java.nio.file.Files#delete(Path)} from {@link AutoCloseable#close()}.
+   * {@link java.io.File#delete()} from {@link AutoCloseable#close()}.
    */
   public static class DeleteOnCloseFile implements AutoCloseable {
 
-    private final Path file;
+    private final Path path;
 
     private DeleteOnCloseFile(Path pFile) {
-      file = pFile;
+      path = pFile;
     }
 
     public Path toPath() {
-      return file;
+      return path;
     }
 
     @Override
     public void close() throws IOException {
-      java.nio.file.Files.delete(file);
+      path.toFile().delete();
     }
   }
 
   /**
-   * Try to delete a file by calling {@link java.nio.file.Files#delete(Path)}.
+   * Try to delete a file.
    *
    * If deleting fails, and an exception is given as second parameter,
    * the exception from the deletion is added to the given exception,
@@ -145,14 +139,17 @@ public final class Files {
    * }
    * </code>
    *
-   * @param file The file to delete.
+   * @param path The file to delete.
    * @param pendingException An optional pending exception.
    * @throws IOException If deletion fails and no pending exception was given.
    */
-  public static void delete(Path file, @Nullable IOException pendingException) throws IOException {
-    try {
-      java.nio.file.Files.delete(file);
-    } catch (IOException deleteException) {
+  public static void delete(Path path, @Nullable IOException pendingException) throws IOException {
+    File file = path.toFile();
+    boolean deletionSucceeded = file.delete();
+
+    if (!deletionSucceeded) {
+      IOException deleteException = new IOException("The file " + file + " could not be deleted.");
+
       if (pendingException != null) {
         pendingException.addSuppressed(deleteException);
       } else {
@@ -168,7 +165,7 @@ public final class Files {
    * @throws IOException
    */
   public static void writeFile(File file, Object content) throws IOException {
-    writeFile(file.toPath(), content);
+    writeFile(Path.fromFile(file), content);
   }
 
   /**
@@ -186,33 +183,30 @@ public final class Files {
 
   /**
    * Open a BufferedWriter to a file with the default charset.
-   * In addition to {@link java.nio.file.Files#newBufferedWriter(Path, Charset, OpenOption...)},
-   * this method creates necessary parent directories first.
+   * This method creates necessary parent directories beforehand.
    *
    * Note that using the default charset is often not a good idea,
    * because it varies from platform to platform.
-   * Consider using {@link #openOutputFile(Path, Charset, OpenOption...)}
-   * and explicitly specifying a charset.
+   * Consider explicitly specifying a charset.
    *
    * TODO should we use UTF8 here instead?
    */
-  public static BufferedWriter openOutputFile(Path file, OpenOption... options) throws IOException {
+  public static BufferedWriter openOutputFile(Path file, FileWriteMode... options) throws IOException {
     return openOutputFile(file, Charset.defaultCharset(), options);
   }
 
   /**
    * Open a BufferedWriter to a file.
-   * In addition to {@link java.nio.file.Files#newBufferedWriter(Path, Charset, OpenOption...)},
-   * this method creates necessary parent directories first.
+   * This method creates necessary parent directories beforehand.
    */
   public static BufferedWriter openOutputFile(Path file, Charset charset,
-      OpenOption... options) throws IOException {
+      FileWriteMode... options) throws IOException {
     Path dir = file.getParent();
     if (dir != null) {
-      java.nio.file.Files.createDirectories(dir);
+      dir.toFile().mkdirs();
     }
 
-    return java.nio.file.Files.newBufferedWriter(file, charset, options);
+    return (BufferedWriter) file.asCharSink(charset, options).openBufferedStream();
   }
 
   /**
@@ -223,7 +217,7 @@ public final class Files {
    * @throws IOException
    */
   public static void appendToFile(File file, Object content) throws IOException {
-    appendToFile(file.toPath(), content);
+    appendToFile(Path.fromFile(file), content);
   }
 
   /**
@@ -235,7 +229,7 @@ public final class Files {
    */
   public static void appendToFile(Path file, Object content) throws IOException {
     checkNotNull(content);
-    try (Writer w = openOutputFile(file, APPEND, CREATE)) {
+    try (Writer w = openOutputFile(file, FileWriteMode.APPEND)) {
       Appenders.appendTo(w, content);
     }
   }
@@ -248,7 +242,7 @@ public final class Files {
    * @throws FileNotFoundException If one of the conditions is not true.
    */
   public static void checkReadableFile(File file) throws FileNotFoundException {
-    checkReadableFile(file.toPath());
+    checkReadableFile(org.sosy_lab.common.Path.fromFile(file));
   }
 
 
@@ -256,22 +250,22 @@ public final class Files {
    * Verifies if a file exists, is a normal file and is readable. If this is not
    * the case, a FileNotFoundException with a nice message is thrown.
    *
-   * @param file The file to check.
+   * @param path The file to check.
    * @throws FileNotFoundException If one of the conditions is not true.
    */
-  public static void checkReadableFile(Path file) throws FileNotFoundException {
-    Preconditions.checkNotNull(file);
+  public static void checkReadableFile(org.sosy_lab.common.Path path) throws FileNotFoundException {
+    Preconditions.checkNotNull(path);
 
-    if (!java.nio.file.Files.exists(file)) {
-      throw new FileNotFoundException("File " + file.toAbsolutePath() + " does not exist!");
+    if (!path.toFile().exists()) {
+      throw new FileNotFoundException("File " + path.toAbsolutePath() + " does not exist!");
     }
 
-    if (!java.nio.file.Files.isRegularFile(file)) {
-      throw new FileNotFoundException("File " + file.toAbsolutePath() + " is not a normal file!");
+    if (!path.toFile().isFile()) {
+      throw new FileNotFoundException("File " + path.toAbsolutePath() + " is not a normal file!");
     }
 
-    if (!java.nio.file.Files.isReadable(file)) {
-      throw new FileNotFoundException("File " + file.toAbsolutePath() + " is not readable!");
+    if (!path.toFile().canRead()) {
+      throw new FileNotFoundException("File " + path.toAbsolutePath() + " is not readable!");
     }
   }
 }
--- src/org/sosy_lab/common/configuration/Configuration.java
+++ src/org/sosy_lab/common/configuration/Configuration.java
@@ -33,8 +33,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -55,6 +53,7 @@ import org.sosy_lab.common.Classes;
 import org.sosy_lab.common.Classes.UnexpectedCheckedException;
 import org.sosy_lab.common.LogManager;
 import org.sosy_lab.common.Pair;
+import org.sosy_lab.common.Path;
 import org.sosy_lab.common.configuration.converters.BaseTypeConverter;
 import org.sosy_lab.common.configuration.converters.ClassTypeConverter;
 import org.sosy_lab.common.configuration.converters.IntegerTypeConverter;
@@ -117,7 +116,7 @@ public class Configuration {
       setupProperties();
 
       properties.put(name, value);
-      sources.put(name, Paths.get("manually set"));
+      sources.put(name, new Path("manually set"));
 
       return this;
     }
@@ -144,7 +143,7 @@ public class Configuration {
 
       properties.putAll(options);
       for (String name : options.keySet()) {
-        sources.put(name, Paths.get("manually set"));
+        sources.put(name, new Path("manually set"));
       }
 
       return this;
