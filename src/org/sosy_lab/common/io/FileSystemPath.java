ac12691c5cf7b9227963d87c1992b3ca1dc3a514
dresler <dresler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2014-01-13 12:25:34 +0000

    Implement equals() and hashCode() to allow equality check on path names.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@377 05a39ec8-3e86-40da-8f8d-20173f296dbc

--- src/org/sosy_lab/common/io/FileSystemPath.java
+++ src/org/sosy_lab/common/io/FileSystemPath.java
@@ -229,4 +229,29 @@ public class FileSystemPath implements Path {
   public String toString() {
     return toFile().toString();
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((path == null) ? 0 : path.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    FileSystemPath other = (FileSystemPath) obj;
+    if (path == null) {
+      if (other.path != null)
+        return false;
+    } else if (!path.equals(other.path))
+      return false;
+    return true;
+  }
 }
