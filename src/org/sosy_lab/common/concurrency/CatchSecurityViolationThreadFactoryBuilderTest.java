4fe116704680b1b7cc22d2c83b7376387a4ed539
dresler <dresler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2014-01-19 12:43:24 +0000

    Add an implementation of a ThreadFactory builder that catches SecurityException.
    Google App Engine does not allow modifying a Thread instance. I.e. setting its
    name and priority or making it a daemon. Any attempts doing so will raise a
    SecurityException and will therefore stop execution on App Engine. The new
    builder is basically a re-implementation of Google Guava's ThreadFactoryBuilder.
    However silently catching any SecurityException.
    
    
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@386 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/concurrency/CatchSecurityViolationThreadFactoryBuilderTest.java
@@ -0,0 +1,87 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.common.concurrency;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.ThreadFactory;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CatchSecurityViolationThreadFactoryBuilderTest {
+
+  private CatchSecurityViolationThreadFactoryBuilder builder;
+  private Runnable mockRunnable = mock(Runnable.class);
+
+  @Before
+  public void setUp() {
+    builder = new CatchSecurityViolationThreadFactoryBuilder();
+  }
+
+  @Test
+  public void shouldSetDaemon() throws Exception {
+    Thread t = builder.setDaemon(true).build().newThread(mockRunnable);
+    assertTrue(t.isDaemon());
+  }
+
+  @Test
+  public void shouldUseNameFormat() throws Exception {
+    ThreadFactory f = builder.setNameFormat("test-%d").build();
+    Thread t1 = f.newThread(mockRunnable);
+    Thread t2 = f.newThread(mockRunnable);
+
+    assertEquals("test-0", t1.getName());
+    assertEquals("test-1", t2.getName());
+  }
+
+  @Test
+  public void shouldSetPriority() throws Exception {
+    Thread t = builder.setPriority(Thread.MAX_PRIORITY).build().newThread(mockRunnable);
+    assertEquals(Thread.MAX_PRIORITY, t.getPriority());
+  }
+
+  @Test
+  public void shouldSetExceptionHandler() throws Exception {
+    UncaughtExceptionHandler mockHandler = mock(UncaughtExceptionHandler.class);
+    Thread t = builder.setUncaughtExceptionHandler(mockHandler).build().newThread(mockRunnable);
+    assertEquals(mockHandler, t.getUncaughtExceptionHandler());
+  }
+
+  @Test
+  public void shouldUseCustomThreadFactory() throws Exception {
+    ThreadFactory stubFactory = mock(ThreadFactory.class);
+    Thread mockThread = mock(Thread.class);
+    when(stubFactory.newThread(any(Runnable.class))).thenReturn(mockThread);
+
+    Thread t = builder.setThreadFactory(stubFactory).build().newThread(mockRunnable);
+
+    Assert.assertEquals(mockThread, t);
+  }
+}
