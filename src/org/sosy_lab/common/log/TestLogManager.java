9d01abc916552a9c167eb705506e7071721e6609
pwendler <pwendler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2014-03-03 15:30:18 +0000

    New LogManager implementation specifically for testing.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@406 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/log/TestLogManager.java
@@ -0,0 +1,96 @@
+/*
+ *  SoSy-Lab Common is a library of useful utilities.
+ *  This file is part of SoSy-Lab Common.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.sosy_lab.common.log;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.logging.Level;
+
+import javax.annotation.Nullable;
+
+/**
+ * LogManager implementation intended for testing
+ * when nothing should actually be logged.
+ *
+ * However, it does check all the parameters for validity,
+ * i.e. non-nullness and correct string format.
+ */
+public enum TestLogManager implements LogManager {
+  @Deprecated
+  // Use getInstance() instead, enum is just an implementation detail.
+  INSTANCE;
+
+  public static LogManager getInstance() {
+    return INSTANCE;
+  }
+
+  @Override
+  public boolean wouldBeLogged(Level pPriority) {
+    checkNotNull(pPriority);
+
+    // Return true such that no calls to log() will be omitted
+    // (they are important for checking parameter validity).
+    return true;
+  }
+
+  @Override
+  public void log(Level pPriority, Object... pArgs) {
+    checkNotNull(pPriority);
+    checkNotNull(pArgs);
+  }
+
+  @Override
+  public void logf(Level pPriority, String pFormat, Object... pArgs) {
+    checkNotNull(pPriority);
+    checkNotNull(pFormat);
+    checkNotNull(pArgs);
+    String.format(pFormat, pArgs);
+  }
+
+  @Override
+  public void logUserException(Level pPriority, Throwable pE, @Nullable String pAdditionalMessage) {
+    checkNotNull(pPriority);
+    checkNotNull(pE);
+  }
+
+  @Override
+  public void logDebugException(Throwable pE, @Nullable String pAdditionalMessage) {
+    checkNotNull(pE);
+  }
+
+  @Override
+  public void logDebugException(Throwable pE) {
+    checkNotNull(pE);
+  }
+
+  @Override
+  public void logException(Level pPriority, Throwable pE, @Nullable String pAdditionalMessage) {
+    checkNotNull(pPriority);
+    checkNotNull(pE);
+  }
+
+  @Override
+  public void flush() {
+  }
+
+  @Override
+  public void close() {
+  }
+}
