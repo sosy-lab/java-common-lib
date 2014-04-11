5902b0695a30f7ac236a18ccab63e4dbe1482b2b
pwendler <pwendler@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2014-04-11 10:35:47 +0000

    Use existing utility method instead of writing it on our own (with bad handling of closeables).
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@421 05a39ec8-3e86-40da-8f8d-20173f296dbc

--- src/org/sosy_lab/common/io/Files.java
+++ src/org/sosy_lab/common/io/Files.java
@@ -25,7 +25,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Nullable;
@@ -237,17 +236,8 @@ public final class Files {
     }
   }
 
-  @SuppressWarnings("resource")
   public static List<String> readLines(Path path, Charset charset) throws IOException {
-    List<String> lines = new ArrayList<>();
-    BufferedReader reader = path.asCharSource(charset).openBufferedStream();
-    String line;
-    while ((line = reader.readLine()) != null) {
-      lines.add(line);
-    }
-    reader.close();
-
-    return lines;
+    return path.asCharSource(charset).readLines();
   }
 
   /**
