ff134dc1051cd6b0d3ab93511e27634f4763fabf
Philipp Wendler <uni@philippwendler.de>
2014-09-08 08:26:49 +0000

    Additional annotations for non-nullness that are missing in javax.applications.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@481 05a39ec8-3e86-40da-8f8d-20173f296dbc

---
+++ src/org/sosy_lab/common/annotations/FieldsAreNonnullByDefault.java
@@ -0,0 +1,44 @@
+/*
+ *  SoSy-Lab Common is a library of useful utilities.
+ *  This file is part of SoSy-Lab Common.
+ *
+ *  Copyright (C) 2007-2014  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.sosy_lab.common.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+/**
+ * An annotation similar to {@link @javax.annotation.ParametersAreNonnullByDefault}
+ * that defines that all methods inside the annotated element
+ * do not return null, unless this is overridden with another method.
+ *
+ * It is defined here because the annotation supplied by FindBugs
+ * is deprecated: {@link edu.umd.cs.findbugs.annotations.ReturnValuesAreNonnullByDefault}.
+ */
+@Documented
+@Nonnull
+@TypeQualifierDefault(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldsAreNonnullByDefault {
+
+}
