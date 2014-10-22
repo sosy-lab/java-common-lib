95abae376b796a79098545805c16886a4544185b
Philipp Wendler <uni@philippwendler.de>
2014-10-22 14:02:57 +0000

    Allow "::" in paths even in safe mode FileTypeConverter, because CPAchecker needs this.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@498 05a39ec8-3e86-40da-8f8d-20173f296dbc

--- src/org/sosy_lab/common/configuration/converters/FileTypeConverterTest.java
+++ src/org/sosy_lab/common/configuration/converters/FileTypeConverterTest.java
@@ -95,6 +95,9 @@ public class FileTypeConverterTest {
           // path and whether it is allowed in safe mode and when included from config/file
           {"/etc/passwd", false, false},
           {"relative/dir" + File.pathSeparator + "/etc", false, false},
+          {"file::name", true, true},
+          {"file::name:illegal", false, false},
+          {"file:::illegal", false, false},
           {"file", true, true},
           {"dir/../file", true, true},
           {"./dir/file", true, true},
