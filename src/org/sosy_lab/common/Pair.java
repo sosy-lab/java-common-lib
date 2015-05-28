049fca4fe18842865fb91594983db4ca5a4d609d
karpenkov <karpenkov@05a39ec8-3e86-40da-8f8d-20173f296dbc>
2015-05-28 13:33:08 +0000

    Adds non-null getters to pair. First step towards null-free pairs.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@517 05a39ec8-3e86-40da-8f8d-20173f296dbc

--- src/org/sosy_lab/common/Pair.java
+++ src/org/sosy_lab/common/Pair.java
@@ -69,6 +69,22 @@ public class Pair<A, B> implements Serializable {
     return second;
   }
 
+  /**
+   * Get the first parameter, crash if it is null.
+   */
+  public A getFirstNotNull() {
+    checkNotNull(first);
+    return first;
+  }
+
+  /**
+   * Get the second parameter, crash if it is null.
+   */
+  public B getSecondNotNull() {
+    checkNotNull(second);
+    return second;
+  }
+
   @Override
   public String toString() {
     return "(" + first + ", " + second + ")";
@@ -140,7 +156,7 @@ public class Pair<A, B> implements Serializable {
           @Override
           public Pair<T, T2> apply(
               @Nonnull Entry<? extends T, ? extends T2> pArg0) {
-            return Pair.<T, T2>of(pArg0.getKey(), pArg0.getValue());
+            return Pair.of(pArg0.getKey(), pArg0.getValue());
           }
         };
   }
@@ -154,7 +170,7 @@ public class Pair<A, B> implements Serializable {
     while (iteratorA.hasNext()) {
       checkArgument(iteratorB.hasNext(), "Second list is shorter");
 
-      result.add(Pair.<A, B>of(iteratorA.next(), iteratorB.next()));
+      result.add(Pair.of(iteratorA.next(), iteratorB.next()));
     }
     checkArgument(!iteratorB.hasNext(), "Second list is longer");
 
@@ -223,7 +239,7 @@ public class Pair<A, B> implements Serializable {
     return new Function<Pair<A1, A2>, Pair<B1, B2>>() {
       @Override
       public Pair<B1, B2> apply(@Nonnull Pair<A1, A2> pInput) {
-        return Pair.<B1, B2>of(f1.apply(pInput.getFirst()),
+        return Pair.of(f1.apply(pInput.getFirst()),
                                f2.apply(pInput.getSecond()));
       }
     };
