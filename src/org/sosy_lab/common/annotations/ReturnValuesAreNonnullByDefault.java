c7cb6a12c837ef7346c22552e0e435195799c865
Philipp Wendler <uni@philippwendler.de>
2015-09-18 09:14:27 +0000

    Fix annotations that specified to annotate different elements than their names said.
    git-svn-id: https://svn.sosy-lab.org/software/common/trunk@540 05a39ec8-3e86-40da-8f8d-20173f296dbc

--- src/org/sosy_lab/common/annotations/ReturnValuesAreNonnullByDefault.java
+++ src/org/sosy_lab/common/annotations/ReturnValuesAreNonnullByDefault.java
@@ -30,14 +30,14 @@ import javax.annotation.meta.TypeQualifierDefault;
 /**
  * An annotation similar to {@link @javax.annotation.ParametersAreNonnullByDefault}
  * that defines that all methods inside the annotated element
- * do not return null, unless this is overridden with another method.
+ * do not return null, unless this is overridden with another annotation.
  *
  * It is defined here because the annotation supplied by FindBugs
  * is deprecated: {@link edu.umd.cs.findbugs.annotations.ReturnValuesAreNonnullByDefault}.
  */
 @Documented
 @Nonnull
-@TypeQualifierDefault(ElementType.FIELD)
+@TypeQualifierDefault(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ReturnValuesAreNonnullByDefault {
 
