8f1e7bed69f1732c75dc1d60a16c2e5213ad575e
Philipp Wendler <uni@philippwendler.de>
2016-12-16 14:22:25 +0100

    Attemp to fix failing Travis deploy script.

--- build/deploy-gh-pages.sh
+++ build/deploy-gh-pages.sh
@@ -6,12 +6,12 @@ mkdir -p gh-pages/api
 cp -r website/* gh-pages
 cp -r Javadoc/* gh-pages/api
 
+# we need to tell git who we are
+git config --global user.name "${GIT_NAME}"
+git config --global user.email "${GIT_EMAIL}"
+
 cd gh-pages
 git init
-
-# inside this git repo we'll pretend to be a new user
-git config user.name "${GIT_NAME}"
-git config user.email "${GIT_EMAIL}"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
