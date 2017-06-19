8d95d53af99fab32319be3f7a4d2b949c9d4c336
Philipp Wendler <uni@philippwendler.de>
2017-06-19 10:43:36 +0200

    Improve CopyOnWriteSortedMapTest:
    we want to test our map for all features, just not its collection views.

--- src/org/sosy_lab/common/collect/CopyOnWriteSortedMapTest.java
+++ src/org/sosy_lab/common/collect/CopyOnWriteSortedMapTest.java
@@ -19,22 +19,112 @@
  */
 package org.sosy_lab.common.collect;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.testing.AbstractTester;
+import com.google.common.collect.testing.FeatureSpecificTestSuiteBuilder;
+import com.google.common.collect.testing.MapTestSuiteBuilder;
+import com.google.common.collect.testing.OneSizeTestContainerGenerator;
 import com.google.common.collect.testing.SortedMapTestSuiteBuilder;
+import com.google.common.collect.testing.TestMapGenerator;
 import com.google.common.collect.testing.TestStringSortedMapGenerator;
 import com.google.common.collect.testing.features.CollectionFeature;
 import com.google.common.collect.testing.features.CollectionSize;
+import com.google.common.collect.testing.features.Feature;
 import com.google.common.collect.testing.features.MapFeature;
-import com.google.common.collect.testing.testers.MapClearTester;
 import com.google.common.collect.testing.testers.MapEntrySetTester;
-import com.google.common.collect.testing.testers.MapPutAllTester;
-import com.google.common.collect.testing.testers.MapPutTester;
-import com.google.common.collect.testing.testers.MapRemoveTester;
+import com.google.common.collect.testing.testers.MapReplaceAllTester;
 import com.google.common.collect.testing.testers.SortedMapNavigationTester;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.stream.Stream;
+import junit.framework.TestSuite;
 
 public class CopyOnWriteSortedMapTest {
+
+  /** A delegating {@link FeatureSpecificTestSuiteBuilder} that overrides the set of features. */
+  private static class FeatureOverrideTestSuiteBuilder<K, V>
+      extends FeatureSpecificTestSuiteBuilder<
+          FeatureOverrideTestSuiteBuilder<K, V>,
+          OneSizeTestContainerGenerator<Map<K, V>, Entry<K, V>>> {
+
+    private final FeatureSpecificTestSuiteBuilder<
+            ?, ? extends OneSizeTestContainerGenerator<Map<K, V>, Entry<K, V>>>
+        delegate;
+    private final Set<Feature<?>> features;
+
+    FeatureOverrideTestSuiteBuilder(
+        FeatureSpecificTestSuiteBuilder<
+                ?, ? extends OneSizeTestContainerGenerator<Map<K, V>, Entry<K, V>>>
+            pDelegate,
+        Set<Feature<?>> pFeatures) {
+      delegate = pDelegate;
+      features = ImmutableSet.copyOf(pFeatures);
+    }
+
+    @Override
+    public Set<Feature<?>> getFeatures() {
+      return features;
+    }
+
+    @Override
+    public String getName() {
+      return delegate.getName();
+    }
+
+    @Override
+    public Set<Method> getSuppressedTests() {
+      return delegate.getSuppressedTests();
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    protected List<Class<? extends AbstractTester>> getTesters() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public OneSizeTestContainerGenerator<Map<K, V>, Entry<K, V>> getSubjectGenerator() {
+      return delegate.getSubjectGenerator();
+    }
+  }
+
+  /**
+   * A {@link SortedMapTestSuiteBuilder} that ensures that submaps, entry set etc. are not tested
+   * for mutability.
+   */
+  private static class UnmodifiableViewSortedMapTestSuiteBuilder<K, V>
+      extends SortedMapTestSuiteBuilder<K, V> {
+
+    @Override
+    protected MapTestSuiteBuilder<K, V> usingGenerator(TestMapGenerator<K, V> pSubjectGenerator) {
+      return super.usingGenerator(pSubjectGenerator);
+    }
+
+    @Override
+    protected List<TestSuite> createDerivedSuites(
+        FeatureSpecificTestSuiteBuilder<
+                ?, ? extends OneSizeTestContainerGenerator<Map<K, V>, Entry<K, V>>>
+            pParentBuilder) {
+
+      Set<Feature<?>> features = new HashSet<>(pParentBuilder.getFeatures());
+      features.remove(MapFeature.GENERAL_PURPOSE);
+      features.remove(MapFeature.SUPPORTS_PUT);
+      features.remove(MapFeature.SUPPORTS_REMOVE);
+      features.remove(CollectionFeature.REMOVE_OPERATIONS);
+      features.remove(CollectionFeature.SUPPORTS_ADD);
+      features.remove(CollectionFeature.SUPPORTS_REMOVE);
+      features.remove(CollectionFeature.SUPPORTS_ITERATOR_REMOVE);
+      features.remove(CollectionFeature.GENERAL_PURPOSE);
+
+      return super.createDerivedSuites(
+          new FeatureOverrideTestSuiteBuilder<>(pParentBuilder, features));
+    }
+  }
 
   private static final TestStringSortedMapGenerator mapGenerator =
       new TestStringSortedMapGenerator() {
@@ -49,24 +139,23 @@ public class CopyOnWriteSortedMapTest {
       };
 
   public static junit.framework.Test suite() throws NoSuchMethodException, SecurityException {
-    return SortedMapTestSuiteBuilder.using(mapGenerator)
+    // Our collection views are unmodifiable, so we need special TestSuiteBuilder
+    return new UnmodifiableViewSortedMapTestSuiteBuilder<String, String>()
+        .usingGenerator(mapGenerator)
         .named("CopyOnWriteSortedMap")
         .withFeatures(
+            MapFeature.GENERAL_PURPOSE,
             MapFeature.ALLOWS_NULL_VALUES,
-            // MapFeature.GENERAL_PURPOSE Not possible because collection views are unmodifiable
             CollectionFeature.KNOWN_ORDER,
             CollectionSize.ANY)
 
         // We throw ClassCastException as allowed by the JavaDoc of SortedMap
         .suppressing(MapEntrySetTester.class.getMethod("testContainsEntryWithIncomparableKey"))
 
-        // Map is actually mutable, can't select the appropriate tests (see above)
-        .suppressing(MapPutTester.class.getMethod("testPut_unsupportedNotPresent"))
-        .suppressing(MapPutTester.class.getMethod("testPut_unsupportedPresentDifferentValue"))
-        .suppressing(MapPutAllTester.class.getMethod("testPutAll_unsupportedSomePresent"))
-        .suppressing(MapPutAllTester.class.getMethod("testPutAll_unsupportedNonePresent"))
-        .suppressing(MapRemoveTester.class.getMethod("testRemove_unsupported"))
-        .suppressing(MapClearTester.class.getMethod("testClear_unsupported"))
+        // We do not support Map.Entry.setValue()
+        .suppressing(MapEntrySetTester.class.getMethod("testSetValue"))
+        .suppressing(MapReplaceAllTester.class.getMethod("testReplaceAllPreservesOrder"))
+        .suppressing(MapReplaceAllTester.class.getMethod("testReplaceAllRotate"))
 
         // subMap is created lazily
         // TODO change this and enable test
