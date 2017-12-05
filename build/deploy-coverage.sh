be82bdf230ad7a5a7e54287a974da93943e20ed5
Philipp Wendler <uni@philippwendler.de>
2017-12-05 11:28:09 +0100

    Better debugging for failures of deploy-coverage.sh

--- build/deploy-coverage.sh
+++ build/deploy-coverage.sh
@@ -10,7 +10,14 @@ fi
 
 # From https://github.com/codacy/codacy-coverage-reporter#travis-ci
 # The option --insecure might help against GitHub-API-issues, causing responses containing "null".
-CODACY_COVERAGE_REPORTER_URL="$(curl -k https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r .assets[0].browser_download_url)"
+CODACY_COVERAGE_REPORTER_RELEASE="$(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest)"
+CODACY_COVERAGE_REPORTER_URL="$(echo $CODACY_COVERAGE_REPORTER_RELEASE | jq -r .assets[0].browser_download_url)"
+if [ "$CODACY_COVERAGE_REPORTER_URL" = "null" ]; then
+  echo "Failed to get information about coday coverage reporter from GitHub:"
+  echo "$CODACY_COVERAGE_REPORTER_RELEASE"
+  exit
+fi
+
 echo "Downloading Codacy Coverage Reporter from $CODACY_COVERAGE_REPORTER_URL"
 wget -O codacy-coverage-reporter-assembly-latest.jar "$CODACY_COVERAGE_REPORTER_URL"
 
