afa590a48c3ea65a3ce2a80374a7ecb452ab3b8f
Philipp Wendler <uni@philippwendler.de>
2017-12-05 12:04:02 +0100

    Use authenticated GitHub request to avoid problems with rate limiting

--- build/deploy-coverage.sh
+++ build/deploy-coverage.sh
@@ -10,7 +10,7 @@ fi
 
 # From https://github.com/codacy/codacy-coverage-reporter#travis-ci
 # The option --insecure might help against GitHub-API-issues, causing responses containing "null".
-CODACY_COVERAGE_REPORTER_RELEASE="$(curl https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest)"
+CODACY_COVERAGE_REPORTER_RELEASE="$(curl -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest)"
 CODACY_COVERAGE_REPORTER_URL="$(echo $CODACY_COVERAGE_REPORTER_RELEASE | jq -r .assets[0].browser_download_url)"
 if [ "$CODACY_COVERAGE_REPORTER_URL" = "null" ]; then
   echo "Failed to get information about coday coverage reporter from GitHub:"
