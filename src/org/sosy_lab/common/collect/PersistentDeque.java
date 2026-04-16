4f148695d9b06cc2179fec70d8e15c56374dccc2
Colleen <elliott@cip.ifi.lmu.de>
2026-04-16 18:54:23 +0200

    Add PersistentDeque class and PersistentDequeInterface

---
+++ src/org/sosy_lab/common/collect/PersistentDeque.java
@@ -0,0 +1,17 @@
+// This file is part of SoSy-Lab Common,
+// a library of useful utilities:
+// https://github.com/sosy-lab/java-common-lib
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.common.collect;
+
+import com.google.errorprone.annotations.Immutable;
+
+@Immutable(containerOf = {})
+public final class PersistentDeque implements PersistentDequeInterface {
+
+
+}
---
+++ src/org/sosy_lab/common/collect/PersistentDequeInterface.java
@@ -0,0 +1,14 @@
+// This file is part of SoSy-Lab Common,
+// a library of useful utilities:
+// https://github.com/sosy-lab/java-common-lib
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.common.collect;
+
+import java.util.Deque;
+
+public interface PersistentDequeInterface extends Deque {
+}
