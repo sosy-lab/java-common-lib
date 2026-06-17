97b7bd394baacd9c827114f58daab63c8d15d84e
Colleen <elliott@cip.ifi.lmu.de>
2026-06-17 16:09:18 +0200

    Add SortedUnionFindTest with setup and first few tests; needs further work though

---
+++ src/org/sosy_lab/common/collect/SortedUnionFindTest.java
@@ -0,0 +1,47 @@
+// This file is part of SoSy-Lab Common,
+// a library of useful utilities:
+// https://github.com/sosy-lab/java-common-lib
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.common.collect;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.collect.Range;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SortedUnionFindTest {
+
+  static final Range<Integer> LOW_NUMS = Range.closed(0, 4);
+  static final Range<Integer> HIGH_NUMS = Range.closed(5, 9);
+
+  static SortedUnionFind<Integer> unionFind = new SortedTreeSetUnionFind<>();
+
+  @BeforeClass
+  public static void setup() {
+    unionFind = new SortedTreeSetUnionFind<>();
+
+    for (int i = 0; i <= 4; i++) {
+      unionFind.union(0, i);
+    }
+    for (int i = 5; i <= 9; i++) {
+      unionFind.union(5, i);
+    }
+  }
+
+  @Test
+  public void testFind_ElementNotContained() {
+    assertThat(LOW_NUMS.contains(unionFind.find(8))).isFalse();
+    assertThat(HIGH_NUMS.contains(unionFind.find(2))).isFalse();
+  }
+
+  @Test
+  public void testFind_ElementContained() {
+    assertThat(LOW_NUMS.contains(unionFind.find(2))).isTrue();
+    assertThat(HIGH_NUMS.contains(unionFind.find(8))).isTrue();
+  }
+}
