493a22053da1701d44205d6da0f29df174668e53
Colleen <elliott@cip.ifi.lmu.de>
2026-06-23 09:35:14 +0200

    Add documentation to SortedUnionFind

--- src/org/sosy_lab/common/collect/SortedUnionFind.java
+++ src/org/sosy_lab/common/collect/SortedUnionFind.java
@@ -11,12 +11,43 @@ package org.sosy_lab.common.collect;
 import java.util.Collection;
 import java.util.Set;
 
+/**
+ * Interface for a sorted Union-Find or Disjoint-Set data structure. Uses a {@link Collection} of
+ * {@link Set}s.
+ *
+ * @param <T> type of elements added to the Union-Find. Must be {@link Comparable} to ensure correct
+ *     ordering.
+ */
 public interface SortedUnionFind<T> {
+  /**
+   * Returns the canonical element of the set containing the provided element.
+   *
+   * @param e element for which set is to be found
+   * @return canonical element of the found set
+   */
   T find(T e);
 
+  /**
+   * Merges the sets represented by the two input values according to standard Union-Find behaviour.
+   *
+   * @param e1 first element
+   * @param e2 second element
+   */
   void union(T e1, T e2);
 
+  /**
+   * Provides a {@link Collection} containing all current subsets.
+   *
+   * @return {@link Collection} containing all current subsets
+   */
   Collection<? extends Set<T>> getAllSubsets();
 
+  /**
+   * Checks whether the provided element is contained in any current subset and returns true or
+   * false accordingly.
+   *
+   * @param e element to be searched for
+   * @return true if contained, false if not
+   */
   boolean contains(T e);
 }
