4dd0356a6b19f5e385bc4557c6bb2729dc9e06b8
Colleen <elliott@cip.ifi.lmu.de>
2026-06-25 15:41:53 +0200

    Add interface PersistentSortedUnionFind

---
+++ src/org/sosy_lab/common/collect/PersistentSortedUnionFind.java
@@ -0,0 +1,29 @@
+// This file is part of SoSy-Lab Common,
+// a library of useful utilities:
+// https://github.com/sosy-lab/java-common-lib
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.common.collect;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+import com.google.errorprone.annotations.DoNotCall;
+import java.util.Map;
+import java.util.NavigableSet;
+
+public interface PersistentSortedUnionFind<T extends Comparable<T>> extends SortedUnionFind<T> {
+
+  @CheckReturnValue
+  Map<T, NavigableSet<T>> unionAndCopy(T e1, T e2);
+
+  /**
+   * @throws UnsupportedOperationException Always.
+   * @deprecated Unsupported operation.
+   */
+  @Deprecated
+  @Override
+  @DoNotCall
+  void union(T e1, T e2);
+}
