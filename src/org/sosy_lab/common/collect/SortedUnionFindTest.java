9334994de6603e2e53c108d13e99f95c3efed6b6
Colleen <elliott@cip.ifi.lmu.de>
2026-07-09 19:10:29 +0200

    Make result comparison more straightforward in SortedUnionFindTest

--- src/org/sosy_lab/common/collect/SortedUnionFindTest.java
+++ src/org/sosy_lab/common/collect/SortedUnionFindTest.java
@@ -105,7 +105,7 @@ public class SortedUnionFindTest {
   @Test
   public void testUnion_Strings() {
     SortedUnionFind<String> unionFindString = new SortedTreeSetUnionFind<>();
-    String expected = ".-1.0.1.2.3.4.5.6.7.8.9.";
+    Integer[] expected = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     for (int i = 0; i <= 2; i++) {
       unionFindString.union(Integer.toString(0), Integer.toString(i));
@@ -136,13 +136,6 @@ public class SortedUnionFindTest {
     unionFindString.union(Integer.toString(0), Integer.toString(9));
     assertThat(unionFindString.getAllSubsets()).hasSize(1);
 
-    @Var String result = ".";
-
-    for (Collection<String> subset : unionFindString.getAllSubsets()) {
-      for (String element : subset) {
-        result = result + (Integer.valueOf(element) + ".");
-      }
-    }
-    assertThat(result).isEqualTo(expected);
+    assertThat(unionFindString.getAllSubsets().iterator().next()).containsExactlyElementsIn(expected).inOrder();
   }
 }
