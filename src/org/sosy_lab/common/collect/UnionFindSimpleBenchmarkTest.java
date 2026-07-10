1898af12289a56f163e3c19075b2359a590ba42b
Colleen <elliott@cip.ifi.lmu.de>
2026-07-10 16:51:19 +0200

    Replace recursion with iterative approach in UnionFindSimpleBenchmarkTest; the maths still needs sorting out though

--- src/org/sosy_lab/common/collect/UnionFindSimpleBenchmarkTest.java
+++ src/org/sosy_lab/common/collect/UnionFindSimpleBenchmarkTest.java
@@ -11,58 +11,93 @@ package org.sosy_lab.common.collect;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Sets;
+import com.google.errorprone.annotations.Var;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import org.junit.Test;
 
 public class UnionFindSimpleBenchmarkTest {
   final int lowerBound = 2;
-  final int factorForComparison = 3;
+  final int factorForComparison = 5;
 
   @Test
   public void unionBigOQuadraticEvaluationTest() {
+    // BEGINNING of 1st loop
     Set<SortedUnionFind<Integer>> allUnionFindsOfFirstLoop = new HashSet<>();
 
     Duration timeBeforeFirstLoop = Duration.ofNanos(System.nanoTime());
-    for (int i = 1; i <= lowerBound; i++) {
-      Set<Integer> values = new HashSet<>();
-      for (int j = 1; j <= i; j++) {
-        values.add(j);
-      }
 
-      allUnionFindsOfFirstLoop.addAll(generateUnionFinds(getPermutations(values)));
-    }
+    List<Set<Set<Integer>>> partitions1 = generatePartitions(lowerBound);
+    transformPartitionsToUnionFind(partitions1, allUnionFindsOfFirstLoop);
+
     Duration timeAfterFirstLoop = Duration.ofNanos(System.nanoTime());
     Duration timeOfFirstLoop = timeAfterFirstLoop.minus(timeBeforeFirstLoop);
+    // END of 1st loop
 
-    final int higherBound = lowerBound * factorForComparison;
+    // BEGINNING of 2nd loop
+    int upperBound = lowerBound * factorForComparison;
     Set<SortedUnionFind<Integer>> allUnionFindsOfSecondLoop = new HashSet<>();
 
     Duration timeBeforeSecondLoop = Duration.ofNanos(System.nanoTime());
-    for (int i = 1; i <= higherBound; i++) {
-      Set<Integer> values = new HashSet<>();
-      for (int j = 1; j <= i; j++) {
-        values.add(j);
-      }
 
-      allUnionFindsOfSecondLoop.addAll(generateUnionFinds(getPermutations(values)));
-    }
+    List<Set<Set<Integer>>> partitions2 = generatePartitions(upperBound);
+    transformPartitionsToUnionFind(partitions2, allUnionFindsOfSecondLoop);
+
     Duration timeAfterSecondLoop = Duration.ofNanos(System.nanoTime());
     Duration timeOfSecondLoop = timeAfterSecondLoop.minus(timeBeforeSecondLoop);
+    // END of 2nd loop
 
     assertThat(timeOfSecondLoop)
         .isLessThan(timeOfFirstLoop.multipliedBy(factorForComparison * factorForComparison));
   }
 
-  private Set<SortedUnionFind<Integer>> generateUnionFinds(Set<Set<Set<Integer>>> pInput) {
-    Preconditions.checkNotNull(pInput);
+  private static List<Set<Set<Integer>>> generatePartitions(int pHighestNumber) {
+    @Var List<Set<Set<Integer>>> allPermutations = new ArrayList<>();
 
-    Set<SortedUnionFind<Integer>> unionFindSet = new HashSet<>();
+    // initialise allPermutations
+    Set<Set<Integer>> init = new HashSet<>();
+    Set<Integer> initSubset = new HashSet<>();
+    initSubset.add(0);
+    init.add(initSubset);
+    allPermutations.add(init);
 
-    for (Set<Set<Integer>> subsetOfSets : pInput) {
+    for (int i = 1; i <= pHighestNumber; i++) {
+
+      List<Set<Set<Integer>>> newSets = new ArrayList<>();
+
+      for (Set<Set<Integer>> existingSet : allPermutations) {
+        for (Set<Integer> existingSubset : existingSet) {
+          Set<Set<Integer>> setWithNumber = new HashSet<>(existingSet);
+          setWithNumber.remove(existingSubset);
+          Set<Integer> subsetWithNumber = new HashSet<>(existingSubset);
+          subsetWithNumber.add(i);
+          setWithNumber.add(subsetWithNumber);
+          newSets.add(setWithNumber);
+        }
+
+        Set<Set<Integer>> currentExistingSet = new HashSet<>(existingSet);
+        Set<Integer> subsetWithCurrentI = new HashSet<>();
+        subsetWithCurrentI.add(i);
+        currentExistingSet.add(subsetWithCurrentI);
+        newSets.add(currentExistingSet);
+      }
+
+      allPermutations = newSets;
+    }
+
+    return allPermutations;
+  }
+
+  private static void transformPartitionsToUnionFind(
+      List<Set<Set<Integer>>> pPartitions, Set<SortedUnionFind<Integer>> pSetOfUnionFinds) {
+    Preconditions.checkNotNull(pPartitions);
+    Preconditions.checkNotNull(pSetOfUnionFinds);
+
+    for (Set<Set<Integer>> subsetOfSets : pPartitions) {
       SortedUnionFind<Integer> unionFind = new SortedTreeSetUnionFind<>();
 
       for (Set<Integer> subSubset : subsetOfSets) {
@@ -78,61 +113,7 @@ public class UnionFindSimpleBenchmarkTest {
         }
       }
 
-      unionFindSet.add(unionFind);
+      pSetOfUnionFinds.add(unionFind);
     }
-
-    return unionFindSet;
-  }
-
-  private Set<Set<Set<Integer>>> getPermutations(Set<Integer> pInput) {
-    Preconditions.checkNotNull(pInput);
-
-    int size = pInput.size();
-    Set<Set<Set<Integer>>> allPermutations = new HashSet<>();
-
-    Set<Set<Integer>> powerSet = Sets.powerSet(pInput);
-
-    for (Set<Integer> currentSet : powerSet) {
-      int freeSlots = size - currentSet.size();
-      Set<Integer> remainingValues = new HashSet<>(pInput);
-      remainingValues.removeAll(currentSet);
-
-      if (currentSet.isEmpty()) {
-        continue;
-      }
-
-      if (freeSlots > 1) {
-        for (Set<Set<Integer>> combinationValues : getPermutations(remainingValues)) {
-          Set<Set<Integer>> subPermutation = new HashSet<>();
-          subPermutation.add(currentSet);
-          subPermutation.addAll(combinationValues);
-          allPermutations.add(subPermutation);
-        }
-      } else if (freeSlots == 1) {
-        Set<Set<Integer>> subPermutation = new HashSet<>();
-        subPermutation.add(currentSet);
-        subPermutation.add(remainingValues);
-        allPermutations.add(subPermutation);
-      } else if (freeSlots == 0) {
-        Set<Set<Integer>> subPermutation = new HashSet<>();
-        subPermutation.add(currentSet);
-        allPermutations.add(subPermutation);
-      }
-    }
-
-    // reduce to those that contain all required numbers
-    Set<Set<Set<Integer>>> filteredPermutations = new HashSet<>(allPermutations);
-    for (Set<Set<Integer>> currentSubset : allPermutations) {
-      int counter = 0;
-
-      for (Set<Integer> currentSubSubset : currentSubset) {
-        for (Integer e : currentSubSubset) {
-          counter++;
-        }
-      }
-      if (!(counter == size)) filteredPermutations.remove(currentSubset);
-    }
-
-    return filteredPermutations;
   }
 }
