344cd5fc7cab5eff1aeb83aaa16bca46b719cc89
Colleen <elliott@cip.ifi.lmu.de>
2026-07-16 19:03:21 +0200

    Add package-info.java to union_find package

---
+++ src/org/sosy_lab/common/collect/union_find/package-info.java
@@ -0,0 +1,17 @@
+// This file is part of SoSy-Lab Common,
+// a library of useful utilities:
+// https://github.com/sosy-lab/java-common-lib
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * This package contains additional interfaces and implementations for collections, as well as
+ * further collection utilities.
+ */
+@com.google.errorprone.annotations.CheckReturnValue
+@javax.annotation.ParametersAreNonnullByDefault
+@org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault
+@org.sosy_lab.common.annotations.FieldsAreNonnullByDefault
+package org.sosy_lab.common.collect.union_find;
