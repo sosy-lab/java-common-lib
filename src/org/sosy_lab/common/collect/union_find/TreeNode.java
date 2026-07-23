a93864c3bee7783dc59c8e42fd13250ec3caa07d
Colleen <elliott@cip.ifi.lmu.de>
2026-07-23 15:37:44 +0200

    Add static creation methods in TreeNode; make parent null in root nodes for now

--- src/org/sosy_lab/common/collect/union_find/TreeNode.java
+++ src/org/sosy_lab/common/collect/union_find/TreeNode.java
@@ -8,12 +8,19 @@
 
 package org.sosy_lab.common.collect.union_find;
 
+import javax.annotation.Nullable;
+
 public class TreeNode<T> {
 
-  TreeNode<T> parent;
+  @Nullable  TreeNode<T> parent;
   T value;
 
-  public TreeNode(TreeNode<T> parent, T value) {
+  private TreeNode(T value) {
+    this.parent = null;
+    this.value = value;
+  }
+
+  private TreeNode(TreeNode<T> parent, T value) {
     this.parent = parent;
     this.value = value;
   }
@@ -24,5 +31,13 @@ public class TreeNode<T> {
 
   public T getValue() {
     return value;
+  }
+
+  public static <V> TreeNode<V> getNewRootNode(V value) {
+    return new TreeNode<V>(value);
+  }
+
+  public static <V> TreeNode<V> getNewNode(TreeNode<V> parent, V value) {
+    return new TreeNode<V>(parent, value);
   }
 }
