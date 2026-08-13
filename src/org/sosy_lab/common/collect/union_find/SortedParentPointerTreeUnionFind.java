8a8f25cea189447bea6b11374d0baaac16c52a0b
Colleen <elliott@cip.ifi.lmu.de>
2026-08-13 14:53:51 +0200

    Add documentation to SortedParentPointerTreeUnionFind

--- src/org/sosy_lab/common/collect/union_find/SortedParentPointerTreeUnionFind.java
+++ src/org/sosy_lab/common/collect/union_find/SortedParentPointerTreeUnionFind.java
@@ -18,6 +18,13 @@ import java.util.TreeSet;
 
 public class SortedParentPointerTreeUnionFind<T> extends ParentPointerTreeUnionFind<T> {
 
+  /**
+   * Provides a {@link Collection} containing all current subsets. It contains the subsets sorted by
+   * their canonical elements in ascending order. The contents of the subsets are also sorted in
+   * ascending order.
+   *
+   * @return sorted {@link Collection} containing all current subsets
+   */
   // subsets are in order of their canonical elements; elements in subsets are sorted as well
   @Override
   public Collection<? extends Set<T>> getAllSubsets() {
