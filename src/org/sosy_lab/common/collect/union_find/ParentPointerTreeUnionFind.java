f0320e40a2996e21d8e4c280493f19f9cf6bcf13
Colleen <elliott@cip.ifi.lmu.de>
2026-08-13 14:50:53 +0200

    Add documentation to ParentPointerTreeUnionFind

--- src/org/sosy_lab/common/collect/union_find/ParentPointerTreeUnionFind.java
+++ src/org/sosy_lab/common/collect/union_find/ParentPointerTreeUnionFind.java
@@ -18,14 +18,30 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+/**
+ * An implementation of {@link UnionFind} using a {@link Map} of {@link ParentPointerTree}s. In
+ * order to represent subsets (the trees) by canonical elements, each one is mapped to its
+ * representative canonical element. This is always the first element added to the subset, unless it
+ * has changed due to union operations. The union is implemented as union by size.
+ *
+ * @param <T> type of elements added to the Union-Find.
+ */
 public class ParentPointerTreeUnionFind<T> implements UnionFind<T> {
 
   protected final Map<T, ParentPointerTree<T>> forest;
 
+  /** Creates an empty instance. */
   public ParentPointerTreeUnionFind() {
     forest = new HashMap<>();
   }
 
+  /**
+   * Returns the canonical element of the set containing the provided element.
+   *
+   * @param value element for which set is to be found
+   * @return canonical element of the found set
+   * @throws IllegalArgumentException if element is not contained in any subset
+   */
   @Override
   public T find(T value) {
 
@@ -40,6 +56,16 @@ public class ParentPointerTreeUnionFind<T> implements UnionFind<T> {
     throw new IllegalArgumentException("Element not contained.");
   }
 
+  /**
+   * Merges the sets represented by the two input values according to standard Union-Find behaviour.
+   *
+   * <p>USES: Add new element as new set: pass it as both value1 and value2. Add new element to
+   * existing set: one input value is the new element, the other the canonical element of the set to
+   * be added to. Merge two existing sets: value1, value2 canonical elements of sets to be merged.
+   *
+   * @param value1 first element
+   * @param value2 second element
+   */
   @Override
   public void union(T value1, T value2) {
 
@@ -69,6 +95,11 @@ public class ParentPointerTreeUnionFind<T> implements UnionFind<T> {
     }
   }
 
+  /**
+   * Provides a {@link Collection} containing all current subsets.
+   *
+   * @return {@link Collection} containing all current subsets
+   */
   @Override
   public Collection<? extends Set<T>> getAllSubsets() {
 
@@ -81,6 +112,13 @@ public class ParentPointerTreeUnionFind<T> implements UnionFind<T> {
     return allSubsets;
   }
 
+  /**
+   * Checks whether the provided element is contained in any current subset and returns true or
+   * false accordingly.
+   *
+   * @param e element to be searched for
+   * @return true if contained, false if not
+   */
   @Override
   public boolean contains(T e) {
 
