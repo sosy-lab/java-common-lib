face617098ca6f934a10005a727bae9ce8b55b1b
Colleen <elliott@cip.ifi.lmu.de>
2026-08-14 14:52:42 +0200

    Introduce AbstractTreeNode class

---
+++ src/org/sosy_lab/common/collect/union_find/AbstractTreeNode.java
@@ -0,0 +1,37 @@
+// This file is part of SoSy-Lab Common,
+// a library of useful utilities:
+// https://github.com/sosy-lab/java-common-lib
+//
+// SPDX-FileCopyrightText: 2026 Dirk Beyer <https://www.sosy-lab.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.sosy_lab.common.collect.union_find;
+
+public abstract class AbstractTreeNode<T> {
+
+  protected AbstractTreeNode<T> parent;
+  protected final T value;
+
+  protected AbstractTreeNode(T value) {
+    this.parent = this;
+    this.value = value;
+  }
+
+  protected AbstractTreeNode(AbstractTreeNode<T> parent, T value) {
+    this.parent = parent;
+    this.value = value;
+  }
+
+  public AbstractTreeNode<T> getParent() {
+    return parent;
+  }
+
+  public void setParent(AbstractTreeNode<T> parent) {
+    this.parent = parent;
+  }
+
+  public T getValue() {
+    return value;
+  }
+}
