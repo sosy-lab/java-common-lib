1f6f4ccbaef22da80cbafece408232e8ae50bf17
Colleen <elliott@cip.ifi.lmu.de>
2026-08-15 17:17:00 +0200

    Increase compatibility between sorted and unsorted union-find versions

--- src/org/sosy_lab/common/collect/union_find/SortedUnionFind.java
+++ src/org/sosy_lab/common/collect/union_find/SortedUnionFind.java
@@ -19,36 +19,13 @@ import java.util.Set;
  * @param <T> type of elements added to the Union-Find. Must be {@link Comparable} to ensure correct
  *     ordering.
  */
-public interface SortedUnionFind<T extends Comparable<T>> {
-  /**
-   * Returns the canonical element of the set containing the provided element.
-   *
-   * @param e element for which set is to be found
-   * @return canonical element of the found set
-   */
-  T find(T e);
-
-  /**
-   * Merges the sets represented by the two input values according to standard Union-Find behaviour.
-   *
-   * @param e1 first element
-   * @param e2 second element
-   */
-  void union(T e1, T e2);
+public interface SortedUnionFind<T extends Comparable<T>> extends UnionFind<T> {
 
   /**
    * Provides a {@link Collection} containing all current subsets.
    *
    * @return {@link Collection} containing all current subsets
    */
+  @Override
   Collection<? extends NavigableSet<T>> getAllSubsets();
-
-  /**
-   * Checks whether the provided element is contained in any current subset and returns true or
-   * false accordingly.
-   *
-   * @param e element to be searched for
-   * @return true if contained, false if not
-   */
-  boolean contains(T e);
 }
